d985c5ce00f31bec68b69b1a276556864fb81bd2
Harmic <hiepthoi2004@gmail.com>
2024-12-10 21:08:25 +0700

    add file ViewComponents

--- Harmic/Controllers/HomeController.cs
+++ Harmic/Controllers/HomeController.cs
@@ -1,20 +1,25 @@
 using System.Diagnostics;
 using Harmic.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Harmic.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly Th2Context _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(Th2Context context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            ViewBag.productCategories = _context.TbProductCategories.ToList();
+            ViewBag.productNew = _context.TbProducts.Where(m => m.IsNew).ToList();
             return View();
         }
 
--- Harmic/obj/Debug/net8.0/Harmic.AssemblyInfo.cs
+++ Harmic/obj/Debug/net8.0/Harmic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38262190590ae16022ff439d77506ac3466340d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Harmic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
